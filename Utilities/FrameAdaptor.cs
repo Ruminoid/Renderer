05b995aa8af2e6ebf4983a68d0acac18992f5759
Il Harper <afanyiyu@hotmail.com>
2020-07-30 21:51:06 +0800

    feat: Add FrameAdaptor Class

---
+++ Utilities/FrameAdaptor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ruminoid.Common.Renderer.Utilities
+{
+    public sealed class FrameAdaptor
+    {
+        #region Structure
+
+        public int FrameRate;
+        public int TotalFrame;
+
+        #endregion
+
+        #region Constructor
+
+        public FrameAdaptor(int frameRate, long total)
+        {
+            FrameRate = frameRate;
+            TotalFrame = (int) Math.Floor((double) total * frameRate / 1000);
+        }
+
+        #endregion
+
+        #region Methods
+
+        public int GetFrameIndex(int milliSec) =>
+            (int) Math.Floor((double) milliSec * FrameRate / 1000);
+
+        public int GetMilliSec(int frameIndex) =>
+            (int) Math.Floor((double) frameIndex * 1000 / FrameRate);
+
+        #endregion
+    }
+}
