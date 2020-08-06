3cf49120d4a92cd77dabb58cea85be8a7bb23764
Charlie Jiang <cqjjjzr@126.com>
2020-08-06 17:59:28 +0800

    :sparkles: (LibASS): Enable multi renderer for LibASS

--- Core/AssRenderCore.cs
+++ Core/AssRenderCore.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -8,18 +8,83 @@ using K4os.Compression.LZ4;
 
 namespace Ruminoid.Common.Renderer.Core
 {
+    public sealed class RenderedImage
+    {
+        public static readonly RenderedImage Empty = new RenderedImage();
+        public int Width, Height, Stride;
+        public byte[] Buffer;
+
+        private bool Equals(RenderedImage other)
+        {
+            return Width == other.Width && Height == other.Height && Stride == other.Stride && Equals(Buffer, other.Buffer);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return ReferenceEquals(this, obj) || obj is RenderedImage other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            if (ReferenceEquals(this, Empty)) return 0;
+            unchecked
+            {
+                var hashCode = Width;
+                hashCode = (hashCode * 397) ^ Height;
+                hashCode = (hashCode * 397) ^ Stride;
+                hashCode = (hashCode * 397) ^ (Buffer != null ? Buffer.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+    }
+
+    public class DecodedImage
+    {
+        public int Width, Height;
+        public byte[] Buffer;
+    }
+
+    public sealed class RenderCoreRenderer : IDisposable
+    {
+        private IntPtr _this;
+
+        public RenderCoreRenderer(IntPtr _this)
+        {
+            this._this = _this;
+        }
+
+        public unsafe RenderedImage Render(int width, int height, int milliSec)
+        {
+            ruminoid_rendercore.RuminoidRcRenderFrame(_this, width, height, milliSec);
+            var result = ruminoid_rendercore.RuminoidRcGetResult(_this);
+
+            var image = new RenderedImage {
+                Width = result.Width, Height = result.Height, Stride = result.Stride
+            };
+            image.Buffer = new byte[result.CompressedSize];
+            Marshal.Copy((IntPtr) result.Buffer, image.Buffer, 0, (int)result.CompressedSize);
+
+            return image;
+        }
+
+        public void Dispose()
+        {
+            ruminoid_rendercore.RuminoidRcDestroyRenderContext(_this);
+            _this = IntPtr.Zero;
+        }
+    }
+
     public sealed class AssRenderCore : IDisposable
     {
         #region User Data
 
-        private readonly int _width, _height, _threadCount;
+        private readonly int _width, _height, _glyphMax, _bitmapMax;
 
         #endregion
 
         #region Core Data
 
         private IntPtr _rcContext;
-        private IntPtr[] _renderContexts;
 
         #endregion
 
@@ -29,23 +94,17 @@ namespace Ruminoid.Common.Renderer.Core
             ref string subData,
             int width,
             int height,
-            int threadCount,
             int glyphMax,
             int bitmapMax)
         {
             // Initialize User Data
             _width = width;
             _height = height;
-            _threadCount = threadCount;
+            _glyphMax = glyphMax;
+            _bitmapMax = bitmapMax;
 
             // Initialize Core Data
             _rcContext = ruminoid_rendercore.RuminoidRcNewContext();
-            _renderContexts = new IntPtr[threadCount];
-            for (int i = 0; i < threadCount; i++)
-            {
-                _renderContexts[i] = ruminoid_rendercore.RuminoidRcNewRenderContext(_rcContext);
-                ruminoid_rendercore.RuminoidRcSetCacheLimits(_renderContexts[i], glyphMax, bitmapMax);
-            }
             UpdateSubtitle(ref subData, (ulong)subData.Length);
         }
 
@@ -53,49 +112,34 @@ namespace Ruminoid.Common.Renderer.Core
 
         #region Methods
 
-        public RuminoidImageT Render(int threadIndex, int milliSec)
+        public RenderCoreRenderer CreateRenderer()
         {
-            ruminoid_rendercore.RuminoidRcRenderFrame(_renderContexts[threadIndex], _width, _height, milliSec);
-            return CopyImage(ruminoid_rendercore.RuminoidRcGetResult(_renderContexts[threadIndex]));
-        }
-
-        private static unsafe RuminoidImageT CopyImage(RuminoidImageT img)
-        {
-            RuminoidImageT result = new RuminoidImageT()
-            {
-                CompressedSize = img.CompressedSize,
-                Width = img.Width,
-                Height = img.Height,
-                Stride = img.Stride,
-                Buffer = (byte*)Marshal.AllocHGlobal((int)img.CompressedSize)
-            };
-
-            for (ulong i = 0; i < img.CompressedSize; i++)
-                result.Buffer[i] = img.Buffer[i];
-
-            return result;
+            var context = ruminoid_rendercore.RuminoidRcNewRenderContext(_rcContext);
+            ruminoid_rendercore.RuminoidRcSetCacheLimits(context, _glyphMax, _bitmapMax);
+            return new RenderCoreRenderer(context);
         }
 
         public void UpdateSubtitle(ref string subData, ulong length) =>
             ruminoid_rendercore.RuminoidRcUpdateSubtitle(_rcContext, subData, length);
 
-        public static unsafe IntPtr Decode(RuminoidImageT image)
+        public static (DecodedImage, byte[]) Decode(byte[] buffer, RenderedImage compressedImage)
         {
-            int width = image.Width,
-                height = image.Height,
-                stride = image.Stride;
+            int width = compressedImage.Width,
+                height = compressedImage.Height,
+                stride = compressedImage.Stride;
 
-            byte* result = (byte*) Marshal.AllocHGlobal(width * height * 4);
-
-            LZ4Codec.Decode(image.Buffer, (int)image.CompressedSize, result, height * stride);
-            if (image.Width * 4 != image.Stride)
-                for (int line = 1; line < height; line++)
+            if (buffer == null || buffer.Length < width * stride)
+                buffer = new byte[width * stride];
+            LZ4Codec.Decode(
+                compressedImage.Buffer, 0, compressedImage.Buffer.Length,
+                buffer, 0, buffer.Length);
+            if (width * 4 != stride)
+                for (var line = 0; line < height; line++)
                 {
-                    for (int i = 0; i < width * 4; i++)
-                        result[line * width * 4 + i] = result[line * stride + i];
+                    Array.Copy(buffer, line * stride,
+                        buffer, line * width * 4, width * 4);
                 }
-
-            return (IntPtr) result;
+            return (new DecodedImage { Width = width, Height = height, Buffer = buffer}, buffer);
         }
 
         #endregion
@@ -104,11 +148,8 @@ namespace Ruminoid.Common.Renderer.Core
 
         public void Dispose()
         {
-            for (int i = 0; i < _threadCount; i++)
-                if (_renderContexts[i] != IntPtr.Zero)
-                    ruminoid_rendercore.RuminoidRcDestroyRenderContext(_renderContexts[i]);
-            if (_rcContext != IntPtr.Zero)
-                ruminoid_rendercore.RuminoidRcDestroyContext(_rcContext);
+            ruminoid_rendercore.RuminoidRcDestroyContext(_rcContext); // no need to check
+            _rcContext = IntPtr.Zero;
         }
 
         #endregion
